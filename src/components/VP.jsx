b715684a8eec6199335667e5c48717947c29bb13
Sivanbachar <sivan.bachar@gmail.com>
2026-04-04 12:45:47 -0400

    Delete src directory

--- src/components/VP.jsx
+++
@@ -1,8 +0,0 @@
-export default function VP({ label, desc, height = 280 }) {
-  return (
-    <div className="visual-placeholder" style={{ minHeight: height }}>
-      <span className="vp-label">[ Visual: {label} ]</span>
-      <p className="vp-desc">{desc}</p>
-    </div>
-  )
-}
